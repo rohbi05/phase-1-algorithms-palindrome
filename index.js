33e67ee95c5b5cff65daeede203edd60cfcfbd96
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-07-22 00:39:31 +0300

    complete assighnment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayOfLetters = word.split("");
+  const reverseLetters = arrayOfLetters.reverse();
+  const reverseString = reverseLetters.join("");
+
+  if (word == reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 /* 
   Add your pseudocode here
@@ -10,7 +20,7 @@ function isPalindrome(word) {
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
+// You can run node index.js to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
